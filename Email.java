ed43b6bf77d7c9ccaf4fe15e70379634206fbcc2
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-03 05:38:47 +0530

    [Deepanjan]add. uc1 is solved.

---
+++ Email.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email {
+	public static void main(String args[]) {
+		List<String> email = new ArrayList<String>();
+		email.add("abc@bridgelabz.com");
+		
+
+		
+		String regex = "^abc@([a-z0-9-]+)\\.([a-z]{2,3})$";
+
+		Pattern pattern = Pattern.compile(regex);
+
+		for (String emailId : email) {
+			Matcher matcher = pattern.matcher(emailId);
+			System.out.println(email + " : " + matcher.matches());
+		}
+	}
+}
